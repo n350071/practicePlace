9a791f1b1ed1876984ece804b6b3b98b4ee7e07d
Naoki Ishigaki <ishigaki0515naoki@gmail.com>
2013-12-23 03:43:33 +0900

    ハノイの塔
    試行錯誤中

--- TowerofHanoi.rb
+++ TowerofHanoi.rb
@@ -14,9 +14,9 @@ class TowerofHanoi
 		#l回で解けるとして、そのm回目の状態を表示する
 		#1回目からl回目までの状態を画面に表示する
 	#イメージ
-	#leftpole => 1,3,5,8,9
-	#centerpole => 2,4,6,7
-	#rightpole =>
+	#poleA => 1,3,5,8,9
+	#poleB => 2,4,6,7
+	#poleC =>
 
 	#インスタンス変数
 	#ポール３本はハノイの塔と共に創造される
@@ -24,19 +24,61 @@ class TowerofHanoi
 	#ポールに数字(円盤)を入力しようとすると、一番小さい数字と比較して、乗せてよいかどうかを判定して返す
 	#乗せてよい場合は、入力された数字を追加する
 	def initialize
-		@leftpole
-		@centerpole
-		@rightpole
+		@poleA = []
+		@poleB = []
+		@poleC = []
 	end
 
-	#ポール間を円盤が移動するメソッド
-	#乗せられるかどうかも判定してくれる
-	def moveNumber(fromPole,toPole)
+	# 試行錯誤のためのメソッド
+	# 1.上から n - 1 個目までの円盤を何らかの方法でAからBに移動する。
+	# 2.残った1枚をAからCに移動する。
+	# 3.Bにある円盤を何らかの方法でBからCに移動する。
+	def moveDiskx
+		#1. 1個目までの円盤を何らかの方法でAからBに移動する
+		@poleB.push(@poleA.pop)
+		# 2.残った1枚をAからCに移動する。
+		@poleC.push(@poleA.pop)
+		# 3.Bにある円盤を何らかの方法でBからCに移動する。
+		@poleC.push(@poleB.pop)
+		poles
+	end
+
+	def moveDisk(from,to)
+	end
+
+	#１〜numberまでの数字をポールAにセットするメソッド
+	def setPoleA(number)
+		for i in 1..number
+			@poleA.push(i)
+		end
+	end
+
+	#ゲッターメソッド
+	def poleA
+		@poleA
+	end
+
+	def poleB
+		@poleB
+	end
+
+	def poleC
+		@poleC
+	end
+
+	#初期化メソッド
+	def flash
+		@poleA = []
+		@poleB = []
+		@poleC = []
 	end
 
 
-	#…と、自我流にやってみたが、なんか良くない予感
-
-
-
+	#ポールの状態を画面に表示する便利メソッド
+	def printPoles
+		puts "+++++++++++++++++++++++++++++"
+		print "poleA #{poleA} \n"
+		print "poleB #{poleB} \n"
+		print "poleC #{poleC} \n"
+	end
 end
--- TowerofHanoiTest.rb
+++ TowerofHanoiTest.rb
@@ -3,4 +3,39 @@
 
 require File.expand_path('../TowerofHanoi.rb', __FILE__)
 class TowerofHanoiTest
+	#定数定義
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	SUCESS = "success"
+	ERROR = "error"
+
+	#インスタンス生成
+	th = TowerofHanoi.new()
+	# th.moveDiskx
+
+	# print "poleA #{th.poleA} \n"
+	# print "#{th.printPoles}"
+
+	#ポールへのセットがうまくできていることのテスト
+	th.setPoleA(2)
+	if th.poleA == [1,2] then
+		puts TRUE
+	else
+		puts FALSE
+	end
+	th.flash
+	th.setPoleA(12)
+	if th.poleA == [1,2,3,4,5,6,7,8,9,10,11,12] then
+		puts TRUE
+	else
+		puts FALSE
+	end
+
+	#ポールに円盤を移せるかチェックしながら移動させるメソッドのテスト
+	# puts "ポールの円盤のサイズを確認しながら移動するテスト"
+	# th.moveDisk(@poleA,@poleB)
+	# th.showCurrentSituation
+
+	#
+
 end
